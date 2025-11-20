424c13dae2975e2351bc92e240689619c32080ec
masahiro <m.togasaki1026@gmail.com>
2025-11-21 10:20:23 +1100

    For loop, Exponent function, and 2D list and nested loop

--- test.py
+++ test.py
@@ -173,4 +173,56 @@ friends = ["Kevin", 2, False, "Karen", "Jim"]
 # else:
 #     print("YOU WIN!")
 
+#For loop
+# friends = ["Jim", "Karen", "Kevin"]
+# len(friends)
+# for index in range(5):
+#     if index == 0:
+#         print("First Iteration")
+#     else:
+#         print("Not First")
+
+# Exponent Function
+# def raise_to_power(base_num, pow_num):
+#     result = 1
+#     for index in range(pow_num):
+#         result = result * base_num
+#     return result
 #
+# print(raise_to_power(3, 2))
+
+# 2D list and Nested lo0p
+# number_grid = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9],
+#     [0]
+# ]
+#
+# for row in number_grid:
+#     for col in row:
+#         print(col)
+#
+# print(number_grid[2][1])
+
+# Basic Translator
+# Lion language
+# Vowels -> g
+# ----------------
+#
+# dog -> dgg
+# cat -> cgt
+
+# def translate(phrase):
+#     translation = ""
+#     for letter in phrase:
+#         if letter.lower() in "aeiou":
+#             if letter.isupper():
+#                 translation = translation + "G"
+#             else:
+#                 translation = translation + "g"
+#         else:
+#             translation = translation + letter
+#     return translation
+#
+# print(translate(input("Enter a phrase: ")))
